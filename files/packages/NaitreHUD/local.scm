8c1e75c02f0df3a93006668f3b334c0fa3d7326f
nixpup <zweitlaender@gmail.com>
2026-02-01 01:18:09 +0100

    Made NaitreHUD Guix Package fetch from Git instead of Local Directory

--- files/packages/NaitreHUD/local.scm
+++ files/packages/NaitreHUD/local.scm
@@ -18,22 +18,15 @@
   #:use-module (guix licenses))
 
 
-(define-public naitre
-  (let ((commit "fe813d7b157c2d290a79cc863387800e99f1f1eb")
-        (revision "1"))
+(define-public naitre-git
   (package
     (name "naitre")
-    (version(git-version "1.0.0" revision commit))
-    (source (origin
-             (method git-fetch)
-             (uri (git-reference
-                   (url "https://github.com/nixpup/NaitreHUD")
-                   (commit commit)))
-             (file-name (git-file-name name version))
-             (sha256
-              (base32
-               "1jbkpcrdw1yqfqnijgfchs0w2iy2ncw9a18h5czvsgcfk5hji4fj"))
-             ))
+    (version "git")
+    ;;(source (local-file "." "naitre-checkout"
+    (source (local-file (dirname (current-filename)) "naitre-checkout"
+                        #:recursive? #t
+                        #:select? (or (git-predicate (current-source-directory))
+                                      (const #t))))
     (build-system meson-build-system)
     (arguments
      (list
@@ -56,9 +49,8 @@
                 (call-with-output-file (string-append wayland-sessions "/naitre.desktop")
                   (lambda (port)
                     (format port "[Desktop Entry]
-Encoding=UTF-8
 Name=NaitreHUD
-Comment=NaitreHUD Wayland WM
+Comment=Wayland compositor based on wlroots and scenefx
 Exec=~a/bin/naitre
 Type=Application
 DesktopNames=NaitreHUD
@@ -80,9 +72,9 @@ DesktopNames=NaitreHUD
                   scenefx))
     (native-inputs (list pkg-config wayland-protocols))
     (home-page "https://github.com/nixpup/NaitreHUD")
-    (synopsis "Wayland Compositor and Window Manager")
+    (synopsis "Wayland compositor based on wlroots and scenefx")
     (description "A Wayland compositor based on wlroots and scenefx,
 inspired by dwl but aiming to be more feature-rich.")
-    (license gpl3))))
+    (license gpl3)))
 
-naitre
+naitre-git
